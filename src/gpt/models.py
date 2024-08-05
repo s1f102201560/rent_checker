e053553a1dbde0af64c87e6c0956e0e3c7d30c5b
1F10220079 <s1F102200794@iniad.org>
2024-08-06 00:05:56 +0900

    add ability to upload files

--- src/gpt/models.py
+++ src/gpt/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 class Memo(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField(max_length=1000)
+    file = models.FileField(upload_to='uploads/')
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
--- src/gpt/views.py
+++ src/gpt/views.py
@@ -1,26 +1,48 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Memo
-from django.views import generic
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
+from django.views import generic
+from .models import Memo
+from .forms import MemoForm
+import os
+from django.http import HttpResponseForbidden
+from django.conf import settings
+from django.http import HttpResponse, Http404
+
+base_url = "https://api.openai.iniad.org/api/v1"
+api_key = os.getenv('OPENAI_API_KEY')
+
+# アップロードしたファイルをほかのユーザーから見れなくするコード
+# configの方にurlがある
+def protected_media(request, path):
+    file_path = os.path.join(settings.MEDIA_ROOT, 'uploads', path)
+    memo = get_object_or_404(Memo, file='uploads/' + path)
+    
+    if memo.author != request.user:
+        return HttpResponseForbidden("You do not have permission to access this file.")
+
+    with open(file_path, 'rb') as f:
+        response = HttpResponse(f.read(), content_type="application/octet-stream")
+        response['Content-Disposition'] = f'attachment; filename={os.path.basename(file_path)}'
+        return response
+
 
 class MemoCreateView(LoginRequiredMixin, generic.edit.CreateView):
     model = Memo
-    fields = ['content', 'title', ]
+    form_class = MemoForm
     template_name = 'gpt/article_form.html'
 
+    # ほかのユーザーから編集、削除されないようにしている
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
 class MemoUpdateView(LoginRequiredMixin, generic.edit.UpdateView):
     model = Memo
-    fields = ['content', 'title', ]
+    form_class = MemoForm
     template_name = 'gpt/article_form.html'
 
-    # ほかのユーザから編集されないようにしてる
     def dispatch(self, request, *args, **kwargs):
         obj = self.get_object()
 
@@ -42,7 +64,6 @@ class MemoDeleteView(LoginRequiredMixin, generic.edit.DeleteView):
 
         return super().dispatch(request, *args, **kwargs)
 
-
 class MemoListView(LoginRequiredMixin, generic.ListView):
     model = Memo
     template_name = 'gpt/article_list.html'
@@ -62,3 +83,10 @@ class MemoDetailView(LoginRequiredMixin, generic.DetailView):
             raise PermissionDenied('You do not have permission to view this.')
 
         return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        memo = self.get_object()
+        file_extension = memo.file.name.split('.')[-1].lower()
+        context['is_image'] = file_extension in ['png', 'jpg', 'jpeg']
+        return context
