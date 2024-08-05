ba9f5c19a2c155c72fd0a01f50033dd68073a565
1F10220079 <s1F102200794@iniad.org>
2024-08-05 22:12:38 +0900

    add user control

--- src/gpt/urls.py
+++ src/gpt/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 from . import views
-from .models import Article
-from django.views import generic
- 
+from .models import Memo
+
 app_name = 'gpt'
- 
+
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('create/', views.CreateView.as_view(), name='create'),
-    path('<int:pk>/update/', views.UpdateView.as_view(), name='update'),
-    path('<int:pk>/delete/', views.DeleteView.as_view(), name='delete'),
+    path('', views.MemoListView.as_view(), name='index'),
+    path('<int:pk>/', views.MemoDetailView.as_view(), name='detail'),
+    path('create/', views.MemoCreateView.as_view(), name='create'),
+    path('<int:pk>/update/', views.MemoUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.MemoDeleteView.as_view(), name='delete'),
 ]
--- src/gpt/views.py
+++ src/gpt/views.py
@@ -1,48 +1,63 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Article
+from .models import Memo
 from django.views import generic
-
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
 
-class CreateView(LoginRequiredMixin, generic.edit.CreateView):
-    model = Article
+class MemoCreateView(LoginRequiredMixin, generic.edit.CreateView):
+    model = Memo
     fields = ['content', 'title', ]
+    template_name = 'gpt/article_form.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        return super(CreateView, self).form_valid(form)
+        return super().form_valid(form)
 
-def index(request):
-    return HttpResponse('Hello Django')
-
-def detail(request, id):
-    return HttpResponse('detail ' + str(id))
-
-def create(request):
-    return HttpResponse('create')
-
-class UpdateView(LoginRequiredMixin, generic.edit.UpdateView):
-    model = Article
+class MemoUpdateView(LoginRequiredMixin, generic.edit.UpdateView):
+    model = Memo
     fields = ['content', 'title', ]
- 
+    template_name = 'gpt/article_form.html'
+
     def dispatch(self, request, *args, **kwargs):
         obj = self.get_object()
- 
+
         if obj.author != self.request.user:
             raise PermissionDenied('You do not have permission to edit.')
- 
-        return super(UpdateView, self).dispatch(request, *args, **kwargs)
 
-class DeleteView(LoginRequiredMixin, generic.edit.DeleteView):
-    model = Article
+        return super().dispatch(request, *args, **kwargs)
+
+class MemoDeleteView(LoginRequiredMixin, generic.edit.DeleteView):
+    model = Memo
     success_url = reverse_lazy('gpt:index')
+    template_name = 'gpt/article_confirm_delete.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+
+        if obj.author != self.request.user:
+            raise PermissionDenied('You do not have permission to delete this.')
+
+        return super().dispatch(request, *args, **kwargs)
 
 
-class IndexView(generic.ListView):
-    model = Article
+class MemoListView(LoginRequiredMixin, generic.ListView):
+    model = Memo
+    template_name = 'gpt/article_list.html'
 
-class DetailView(generic.DetailView):
-    model = Article
+    def get_queryset(self):
+        return Memo.objects.filter(author=self.request.user)
+
+class MemoDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Memo
+    template_name = 'gpt/article_detail.html'
+    context_object_name = 'memo'
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+
+        if obj.author != self.request.user:
+            raise PermissionDenied('You do not have permission to view this.')
+
+        return super().dispatch(request, *args, **kwargs)
