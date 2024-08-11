3a50258fabf6b00afa614a89b9b7de079b20cd49
1F10220156 <s1f102201560@iniad.org>
2024-08-11 16:18:44 +0900

    RAGに食わせるためのモデル追加

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app.models import Resource
 
-# Register your models here.
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+  list_display = ('name', 'created_at')
+  list_filter = ('name', 'created_at')
--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
 
-# Create your models here.
+class Resource(models.Model):
+    name = models.CharField(max_length=100)
+    document = models.FileField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
